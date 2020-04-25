53f24af170f91f0c854b1986f6939de2ffcef38a
JoacimNilsen <joacimnilsen1986@gmail.com>
2020-04-25 14:31:42 +0200

    Fixed errors before PR

--- App.js
+++ App.js
@@ -1,71 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import axios from 'axios'
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import ChatList from './components/ChatList/ChatList'
 
 
-const copy = {
-  postChatButton: 'Add chats',
-  noChats: "There does't seem to be any chats here mate.."
-}
-
-const url = 'https://undefined-backend.herokuapp.com/api/chats'
 
 export const App = () => {
-  const [chats, setChats] = useState([])
-  const [loading, setLoading] = useState(false)
-
-  useEffect( () => {
-    const fetchData = async () => {
-      try {
-        setLoading(true)
-        const chats = (await axios.get(url)).data
-        setChats(chats)
-        setLoading(false)
-      } catch (error) {
-        console.error(error)
-        setChats([])
-        setLoading(false)
-      }
-    }
-    fetchData()
-  }, [])
-
-  
-  const postChat = async (name) => {
-    setLoading(true)
-    
-    try {
-      const newChat = (await axios.post(url, { name })).data
-      console.log(newChat)
-      setChats([...chats, newChat])
-      setLoading(false)
-      a
-    } catch(error ) {
-      console.error(error)
-      setLoading(false)
-    }
-  }
-
-  const doPostChat = async () => {
-    postChat('Erisk chat' + Math.random().toString() + '')
-  }
-
-
   return (
     <View>
       <Text>Hej</Text>
-      {loading && (<Text> Weee are loooaaading</Text>)}
-
-      <TouchableOpacity onPress={() => doPostChat()}><Text>{copy.postChatButton}</Text></TouchableOpacity>
-
-      {chats ? (
-        <View>
-          {chats.map(chat => (
-            <Text key={chat._id}>{chat.name}</Text>
-          ))}
-        </View>
-      ) : <Text>{copy.noChats}</Text>}
-      
+      <ChatList />
     </View>
   );
 };
---
+++ components/ChatList/ChatList.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import axios from 'axios'
+
+
+// TODO: Make this into global thing that is shared throughout applicatoin
+// for localization and managment of copy
+const copy = {
+  chatList: {
+    loading: 'Loading chats',
+    postChatButton: 'Add chats',
+    noChats: "There does't seem to be any chats here mate.."
+  } 
+}
+
+const url = 'https://undefined-backend.herokuapp.com/api/chats'
+
+const ChatList = () => {
+  const [chatName, setChatName] = useState('');
+  const [chats, setChats] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  // TODO: Add statemachine
+
+  useEffect( () => {
+    const fetchData = async () => {
+      try {
+        setLoading(true)
+        const chats = (await axios.get(url)).data
+        setChats(chats)
+        setLoading(false)
+      } catch (error) {
+        console.error(error)
+        setChats([])
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
+  
+  const postChat = async () => {
+
+    const name = 'Jocke123' + Math.random().toString() + ''
+    setLoading(true)
+    try {
+      const newChat = (await axios.post(url, { name })).data
+      console.log(newChat)
+      setChats([...chats, newChat])
+      setLoading(false)
+    } catch(error ) {
+      console.error(error)
+      setLoading(false)
+    }
+  }
+
+
+  return (
+    <View>
+      {loading && (<Text>{copy.chatList.loading}</Text>)}
+
+      {/* TODO: Add as input */}
+      <TouchableOpacity onPress={() => postChat()}><Text>{copy.chatList.postChatButton}</Text></TouchableOpacity>
+
+
+      {/* TODO: make this into chatlist component */}
+      {chats ? (
+        <View>
+          {chats.map(chat => (
+            <Text key={chat._id}>{chat.name}</Text>
+          ))}
+        </View>
+      ) : <Text>{copy.chatList.noChats}</Text>}
+    </View>
+  );
+};
+
+
+export default ChatList
