a253d752aa84b061834921bade9a251056321dfc
Erik Pantzar <erik@epantzar.se>
2020-05-12 16:46:55 +0200

    Update App so it doesnt crash

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import ChatList from './components/ChatList/ChatList'
-
-
 export const App = () => {
   return (
     <View>
       <Text>Hej</Text>
-      <ChatList />
     </View>
   );
 };
-
