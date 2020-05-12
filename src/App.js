1a9c39c57e2f3bcceacee1be57d0d80868b9119f
Erik Pantzar <erik@epantzar.se>
2020-05-12 17:14:16 +0200

    Added token context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { TokenProvider } from './context/token';
 
 export const App = () => {
   return (
-    <View>
-      <Text>Hej</Text>
-    </View>
+    <TokenProvider>
+      <View>
+        <Text>Hej</Text>
+      </View>
+    </TokenProvider>
   );
 };
